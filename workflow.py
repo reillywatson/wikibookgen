690b0cfba1d7bbbe42f01016bf4445e27895d26c
Reilly Watson <reillywatson@gmail.com>
2012-12-02 03:48:29 -0500

    Pass in username and password properly.  Write out successful entries to done list.

--- workflow.py
+++ workflow.py
@@ -1,6 +1,7 @@
 import subprocess
 import codecs
 import epub
+from optparse import OptionParser
 
 def get_book_fields(path):
 	book = epub.open_epub(path)
@@ -11,6 +12,13 @@ def get_book_fields(path):
 	summary += '\n'.join(chapterNames)
 	return (title, summary)
 
+parser = OptionParser()
+parser.add_option('--user', dest='userName')
+parser.add_option('--pass', dest='password')
+(options, _) = parser.parse_args()
+userName = options.userName
+password = options.password
+
 cats = codecs.open('cats.txt', 'r', 'utf-8').read().split('\n')
 published = codecs.open('done.txt', 'r', 'utf-8').read().split('\n')
 cats = [a for a in cats if a not in published]
@@ -18,8 +26,13 @@ for cat in cats:
 	print 'processing %s' % cat
 	retcode = subprocess.call(['casperjs', 'main.js', '--cat=%s'%cat])
 	if retcode != 0:
-		print 'failed!'
+		print 'failed to fetch!'
 		continue
-	title, summary = get_book_fields('test.epub')
-	retcode = subprocess.call(['casperjs', 'publish.js', '--title=%s'%title, '--summary=%s'%summary])
-	print 'retcode: %d' % retcode
+	(title, summary) = get_book_fields('test.epub')
+	retcode = subprocess.call(['casperjs', 'publish.js', '--title=%s'%title, '--summary=%s'%summary, '--user=%s'%userName, '--pass=%s'%password])
+	if retcode != 0:
+		print 'failed to publish!'
+		continue
+	with codecs.open('done.txt', 'w', 'utf8') as out:
+		out.write(cat+'\n')
+	print 'success!'
