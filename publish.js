9b328be24b33730ce7b8147953ce2fde9f9cccfb
Reilly Watson <reillywatson@gmail.com>
2012-12-02 06:34:06 -0500

    try to get rid of some of these sleeps

--- publish.js
+++ publish.js
@@ -28,11 +28,7 @@ casper.then(function() {
 });
 
 casper.thenOpen('https://writinglife.kobobooks.com/ebooks#ebooklibrary/authors');
-casper.wait(1000);
-
 casper.thenClick('#booksLibraryNewDraftButton');
-casper.wait(1000);
-
 casper.then(function() {
 	this.echo('creating new book')
 	this.fill('form#metadata', {
@@ -42,33 +38,28 @@ casper.then(function() {
 	}, false);
 	this.click('#editMetdataCategories');
 });
-casper.wait(1000);
+
+casper.waitForText('Reference');
 
 casper.thenClickLabel('Reference & Language');
-casper.wait(1000);
 casper.thenClickLabel('Reference');
-casper.wait(1000);
 casper.thenClickLabel('Encyclopedias');
 casper.wait(1000);
 casper.thenClickLabel('Confirm');
-casper.wait(1000);
-
 casper.thenClick('#cke_contents_synopsis');
 casper.wait(1000);
 casper.thenEvaluate(function(summaryText) {
 	CKEDITOR.instances.synopsis.setData(summaryText);
 }, { summaryText: summary } );
 
-casper.wait(1000);
+casper.wait(3000);
 
 casper.thenClick('#detailsSaveAndNext');
 
-casper.wait(1000);
 casper.waitForText('What you need to know before', null, function() {
 	this.echo('timeout waiting for upload form!');
 	this.capture('timeout.png');
 }, 20*1000);
-casper.wait(1000);
 
 // requires phantomjs 1.8 or higher to work
 casper.page.onFilePicker = function(oldFile) {
@@ -84,47 +75,61 @@ casper.then(function() {
 });
 casper.wait(1000);
 casper.thenClick('#bookContentSubmit');
-casper.wait(1000);
 
-casper.waitForText('Upload complete', null, function() { this.echo('timeout waiting for upload!');this.capture('timeout.png');}, 60*1000);
-casper.wait(1000);
+casper.waitForText('Upload complete', null, function() {
+	this.echo('timeout waiting for upload!');this.capture('timeout.png');
+}, 60*1000);
 
 casper.then(function() {
 	this.echo('upload complete!');
 	this.click('.modalWindow b');
 });
 
-casper.wait(1000);
+casper.wait(3000);
 
 casper.thenClick('#detailsSaveAndNext');
 
-casper.wait(1000);
+casper.waitForText('Apply Digital Rights');
 
 casper.thenClick('#drm');
 
-casper.wait(1000);
+casper.wait(3000);
 
 casper.thenClick('#detailsSaveAndNext');
 
-casper.wait(1000);
+casper.waitForText('Your currency');
 
-casper.thenEvaluate(function() {
-	var priceElem = document.getElementById('defaultPriceInput');
-	priceElem.value = '1.99';
+casper.then(function() {
+	var price = this.evaluate(function() {
+		var priceElem = document.getElementById('defaultPriceInput');
+		priceElem.value = '1.99';
+		return priceElem.value;
+	});
+	if (!price) {
+		this.capture('noprice.png');
+		this.die('setting price failed!', 1);
+	}
 });
 
-casper.wait(1000);
+casper.wait(3000);
 
 casper.thenClick('#detailsSaveAndNext');
 
-casper.wait(1000);
-
+casper.waitForText('ready to start selling');
+casper.wait(3000);
 casper.thenClick('#publishBookSubmit');
-casper.wait(1000);
+casper.wait(3000);
+casper.waitForText('done!', null, function() {
+	var d = new Date();
+	this.capture('pubfail'+d.getTime()+'.png');
+	this.die('publishing timed out!');
+}, 30000);
 
 casper.then(function() {
 	var dialogText = this.fetchText('.modalWindow');
 	if (dialogText.indexOf('done!') < 0) {
+		var d = new Date();
+		this.capture('pubfail'+d.getTime()+'.png');
 		this.die('publishing failed!' + dialogText, 1);
 	}
 });
