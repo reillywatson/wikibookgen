7614bb50a27f4a317d78f91fe54d905e14033cd8
Reilly Watson <reillywatson@gmail.com>
2012-12-02 05:51:44 -0500

    Publishing works now.  Too bad about all these sleeps, this website is terrible.

--- publish.js
+++ publish.js
@@ -28,8 +28,10 @@ casper.then(function() {
 });
 
 casper.thenOpen('https://writinglife.kobobooks.com/ebooks#ebooklibrary/authors');
+casper.wait(1000);
 
 casper.thenClick('#booksLibraryNewDraftButton');
+casper.wait(1000);
 
 casper.then(function() {
 	this.echo('creating new book')
@@ -40,33 +42,95 @@ casper.then(function() {
 	}, false);
 	this.click('#editMetdataCategories');
 });
+casper.wait(1000);
 
 casper.thenClickLabel('Reference & Language');
+casper.wait(1000);
 casper.thenClickLabel('Reference');
+casper.wait(1000);
 casper.thenClickLabel('Encyclopedias');
+casper.wait(1000);
 casper.thenClickLabel('Confirm');
+casper.wait(1000);
 
 casper.thenClick('#cke_contents_synopsis');
+casper.wait(1000);
 casper.thenEvaluate(function(summaryText) {
 	CKEDITOR.instances.synopsis.setData(summaryText);
 }, { summaryText: summary } );
 
+casper.wait(1000);
+
 casper.thenClick('#detailsSaveAndNext');
 
-casper.waitForSelector('#uploadBookContentForm', null, function() {
-	this.echo('timeout!');
+casper.wait(1000);
+casper.waitForText('What you need to know before', null, function() {
+	this.echo('timeout waiting for upload form!');
 	this.capture('timeout.png');
 }, 20*1000);
+casper.wait(1000);
+
+// requires phantomjs 1.8 or higher to work
+casper.page.onFilePicker = function(oldFile) {
+	casper.echo('file picker!');
+	return 'test.epub';
+};
+
+casper.wait(1000);
 
 casper.then(function() {
-	this.fill('form#uploadBookContentForm', {
-		'bookContent' : 'test.epub'
-	}, true);
+	this.echo('uploading...');
+	this.click('#bookContentInput');
 });
+casper.wait(1000);
+casper.thenClick('#bookContentSubmit');
+casper.wait(1000);
+
+casper.waitForText('Upload complete', null, function() { this.echo('timeout waiting for upload!');this.capture('timeout.png');}, 60*1000);
+casper.wait(1000);
 
 casper.then(function() {
-	this.capture('loggedin.png');
+	this.echo('upload complete!');
+	this.click('.modalWindow b');
 });
+
+casper.wait(1000);
+
+casper.thenClick('#detailsSaveAndNext');
+
+casper.wait(1000);
+
+casper.thenClick('#drm');
+
+casper.wait(1000);
+
+casper.thenClick('#detailsSaveAndNext');
+
+casper.wait(1000);
+
+casper.thenEvaluate(function() {
+	var priceElem = document.getElementById('defaultPriceInput');
+	priceElem.value = '1.99';
+});
+
+casper.wait(1000);
+
+casper.thenClick('#detailsSaveAndNext');
+
+casper.wait(1000);
+
+casper.thenClick('#publishBookSubmit');
+casper.wait(1000);
+
+casper.then(function() {
+	var dialogText = this.fetchText('.modalWindow');
+	if (dialogText.indexOf('done!') < 0) {
+		this.die('publishing failed!' + dialogText, 1);
+	}
+});
+
+casper.thenClick('.modalWindow b');
+casper.wait(1000);
 
 casper.run(function() {
 	this.echo('done!').exit();
