329a20fc59f87ab1e3e708c8a5c2baee9d51e0c7
Reilly Watson <reillywatson@gmail.com>
2012-12-01 23:50:41 -0500

    Remove a bunch of debugging junk.  Set the book title properly.

--- main.js
+++ main.js
@@ -2,43 +2,57 @@ var casper = require('casper').create({
     onError: function(self, m) {   // Any "error" level message will be written
         console.log('FATAL:' + m); // on the console output and PhantomJS will
         self.exit();               // terminate
-    },
+    }
 });
 
-var bookTitle = 'African Nomads'
-var categoryUrl = 'African_nomads'
-var urlEncodedCategory='African+nomads'
+var toTitleCase = function(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
 
-casper.start('http://en.wikipedia.org/wiki/Category:'+categoryUrl);
+var categoryUrl = casper.cli.get('cat');
+var bookTitle = categoryUrl.replace(/_/g, ' ');
+bookTitle = toTitleCase(bookTitle);
+var urlEncodedCategory = categoryUrl.replace(/_/g , ' ');
+urlEncodedCategory = encodeURIComponent(urlEncodedCategory);
+
+casper.start('http://en.wikipedia.org/wiki/Category:'+categoryUrl, function() { this.echo('started for category: ' + bookTitle); });
 
 casper.then(function() {
-	this.echo('url: ' + this.getCurrentUrl());
 	this.click('#coll-create_a_book a');
 });
 
 casper.then(function() {
-	this.echo('url: ' + this.getCurrentUrl());
 	this.click('div.collection-button.ok a');
 });
 
 casper.thenOpen('http://en.wikipedia.org/w/index.php?title=Special:Book&bookcmd=add_category&cattitle='+urlEncodedCategory);
 
 casper.then(function() {
-	this.echo('url: ' + this.getCurrentUrl());
 	this.click('a.collection-creatorbox-iconlink');
+});
+
+casper.then(function() {
+	this.fill('form#mw-collection-title-form', {
+		'collectionTitle' : bookTitle
+	}, true);
 });
 
 casper.thenOpen('http://en.wikipedia.org/wiki/Special:Book',
 {
 	method: 'post',
-	data: { 'writer' : 'epub', 'bookcmd' : 'render', 'collectionTitle' : bookTitle}
+	data: { 'collectionTitle' : bookTitle, 'collectionSubtitle' : '', 'bookcmd' : 'set_titles' }
 });
 
-casper.waitForText('Download the file');
+casper.thenOpen('http://en.wikipedia.org/wiki/Special:Book',
+{
+	method: 'post',
+	data: { 'writer' : 'epub', 'bookcmd' : 'render' }
+});
+
+casper.waitForText('Download the file', null, null, 5*60*1000);
 
 casper.then(function() {
-	this.echo('url: ' + this.getCurrentUrl());
-	this.capture('test6.png');
 	var url = this.evaluate(function() {
 		var links = document.getElementsByTagName('a');
 		for (var i = 0; i < links.length; i++) {
